a5c0dd07df878f2f66a35b87643b7c366175afd1
georgens_ja <jan-daniel.georgens@sensus.com>
2015-03-10 15:54:53 +0100

    Fix wrong value
    DEFAULT_AXIS_STEPS_PER_UNIT for ACME rod

--- Firmware/RUMBA/Marlin/Configuration.h
+++ Firmware/RUMBA/Marlin/Configuration.h
@@ -406,7 +406,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,(200.0*16/8),157.5}  // default steps per unit for i3 Berlin
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,((200.0*16)/2.0),157.5}  // default steps per unit for i3 Berlin
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 26, 50}    // (mm/sec) 
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,25,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
